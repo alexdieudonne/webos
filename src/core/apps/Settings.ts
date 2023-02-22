fb1f47817e056f1d998516ed4c707a2ade658068
Charles <charles258@hotmail.fr>
2023-02-22 22:41:25 +0100

    WIP of settings

--- src/core/apps/Settings.ts
+++ src/core/apps/Settings.ts
@@ -3,7 +3,7 @@ import "../scss/settings.scss"
 
 export class Settings extends AppHandler {
     constructor() {
-        super({ icon: "/img/icons/settings.svg", name: "Settings", handler: () => { } });
+        super({ icon: "/img/icons/settings.svg", name: "Settings", handler: saveSettings });
     }
 
     render() {
@@ -81,7 +81,7 @@ export class Settings extends AppHandler {
                     <div class="bloc-label">
                         <h5>Afficher la latence</h5> 
                         <label class="switch">
-                            <input id="latency" type="checkbox">
+                            <input id="network" type="checkbox">
                             <span class="slider round">
                             </span>
                         </label>
@@ -99,12 +99,23 @@ export class Settings extends AppHandler {
                         </label>
                     </div>
                 </section>
+                 <section>
+                    <h3>Weather</h3>
+                    <div class="bloc-label">
+                        <h5>Afficher la la meteo</h5> 
+                        <label class="switch">
+                            <input id="weather" type="checkbox">
+                            <span class="slider round">
+                            </span>
+                        </label>
+                    </div>
+                </section>
             </div>
         `)
     }
 }
-/*
-// TODO: Add a settings manager to save the settings
+
+
 let savedSettings = {
     date: {
         showDay: true,
@@ -118,14 +129,121 @@ let savedSettings = {
         showSecond: true
     },
     network: {
-        showProvider: true,
         showLatency: true,
     },
     battery: {
         showBattery: true,
-        showBatteryPercent: true
+    },
+    weather: {
+        showWeather: true,
     }
 }
-*/
+
+const saveSettings = ()=> {
+
+    if (localStorage.getItem("settings") === null || localStorage.getItem("settings") === undefined) {
+        localStorage.setItem("settings", JSON.stringify(savedSettings));
+    }
+
+    let settings = JSON.parse(localStorage.getItem("settings") as string);
+
+
+    let date = document.getElementById("date") as HTMLInputElement;
+    let day = document.getElementById("day") as HTMLInputElement;
+    let month = document.getElementById("month") as HTMLInputElement;
+    let year = document.getElementById("year") as HTMLInputElement;
+    let hour = document.getElementById("hour") as HTMLInputElement;
+    let minute = document.getElementById("minute") as HTMLInputElement;
+    let second = document.getElementById("second") as HTMLInputElement;
+    let latency = document.getElementById("network") as HTMLInputElement;
+    let battery = document.getElementById("battery") as HTMLInputElement;
+    let weather = document.getElementById("weather") as HTMLInputElement;
+
+    if(!date || !day || !month || !year || !hour || !minute || !second || !latency || !battery) console.log("Error while getting elements");
+
+    date.checked = settings.date.showDate;
+    day.checked = settings.date.showDay;
+    month.checked = settings.date.showMonth;
+    year.checked = settings.date.showYear;
+    hour.checked = settings.time.showHour;
+    minute.checked = settings.time.showMinute;
+    second.checked = settings.time.showSecond;
+    latency.checked = settings.network.showLatency;
+    battery.checked = settings.battery.showBattery;
+    weather.checked = settings.weather.showWeather;
+
+    date.addEventListener("change", () => {
+        settings.date.showDate = date.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    day.addEventListener("change", () => {
+        settings.date.showDay = day.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    month.addEventListener("change", () => {
+        settings.date.showMonth = month.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    year.addEventListener("change", () => {
+        settings.date.showYear = year.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    hour.addEventListener("change", () => {
+        settings.time.showHour = hour.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    minute.addEventListener("change", () => {
+        settings.time.showMinute = minute.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    second.addEventListener("change", () => {
+        settings.time.showSecond = second.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+    });
+
+    latency.addEventListener("change", () => {
+        settings.network.showLatency = latency.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+        if (latency.checked) {
+            document.getElementById("showNetwork").style.visibility = "visible";
+        } else {
+            document.getElementById("showNetwork").style.visibility = "hidden";
+        }
+    });
+
+    battery.addEventListener("change", () => {
+        settings.battery.showBattery = battery.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+        //add hidden visibility to battery percent
+        if (battery.checked) {
+            document.getElementById("showBattery").hidden = false;
+        } else {
+            document.getElementById("showBattery").hidden = true;
+        }
+
+    }); 
+
+    weather.addEventListener("change", () => {
+        settings.weather.showWeather = weather.checked;
+        localStorage.setItem("settings", JSON.stringify(settings));
+        console.log(document.getElementById("showWeather").hidden)
+        //add hidden visibility to battery percent
+        if (weather.checked) {
+            document.getElementById("showWeather").style.visibility = "visible";
+        } else {
+            document.getElementById("showWeather").style.visibility = "hidden";
+        }
+
+    });
+
+
+   
+}
 
 
--- src/core/lib/AppBar.ts
+++ src/core/lib/AppBar.ts
@@ -15,23 +15,23 @@ export default class AppBar extends OS {
                         <p class="current-time"></p>
                     </div>
                     <div class="center-side">
-                        <section class="weather">
+                        <div id="showWeather" class="weather">
                             <span class="weather-temp"></span>
                             <img src="" alt="" class="weather-icon" loading="lazy" data-show>
                             <span class="weather-location"></span>
                             <a class="refresh-btn" onclick="location.reload();">
                                 <img class="refresh-icon" width="24px" height="24px" src="/img/Refresh.png" alt="Refresh icon">
                             </a>
-                        </section>
+                        </div>
                     </div>
 
                     <div class="right-side">
-                        <div class="icon-group network">
+                        <div id="showNetwork" class="icon-group network">
                             <p>Latency: <span id="latency">0</span></p>
                             <span class="network-name">P. Telecom</span>
                             <img id="network-icon" src="/img/Network.png" alt="Network icon" title="Latency: 0 ms">
                         </div>
-                        <div class="battery">
+                        <div id="showBattery" class="battery">
                             <span class="battery-label"></span>
                             <img class="battery-icon" src="/img/Battery.png" alt="Battery icon">
                         </div>
