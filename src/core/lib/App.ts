a07ab3481a818b35364c8971d2b807e5833e8d43
Charles <charles258@hotmail.fr>
2023-02-28 14:41:10 +0100

    Rollback

--- src/core/lib/App.ts
+++ src/core/lib/App.ts
@@ -2,25 +2,19 @@ import Header from "./Header";
 import AppBar from "./AppBar";
 import Body from "./Body";
 import * as apps from '../apps/index'
-//import LockScreen from "./LockScreen";
+import LockScreen from "./LockScreen";
 
 export default class App {
     constructor() {
-        //const locked = sessionStorage.getItem('locked');
+        const locked = sessionStorage.getItem('locked');
         const app = document.querySelector<HTMLDivElement>("#app")!;
         initApp()
-        /*
+        
         app.innerHTML = `
           ${new Header}
           ${new AppBar}
           ${!locked ? new LockScreen : new Body}
-        `;*/
-
-        app.innerHTML = `
-        ${new Header}
-        ${new AppBar}
-        ${new Body}
-      `;
+        `;
     }
 
 }
